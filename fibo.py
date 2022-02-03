8e27e29eb696e17bfbe1708f95268c9f504cf40b
letscodetogather <latestvirus15@gmail.com>
2022-02-04 00:28:32 +0530

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,13 @@
+# def fibo(num):
+#     a = 0
+#     b = 1
+#     cnt = 2
+#     print(a, b)
+#     while(cnt < num):
+#         s = a+b
+#         a=b
+#         b=s
+#         cnt +=1
+#         print(s)
+#
+# fibo(15)
